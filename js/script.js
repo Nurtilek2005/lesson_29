fd8e35c7c65a57c34302b54659d6ed496401634a
kairat <ltdcvet@gmail.com>
2021-01-14 19:25:13 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// let number = 20;
+
+// let square = function (number) {
+//   return number * number;
+// };
+
+// console.log(square(4));
+// console.log(square(5));
+// console.log(number);
+
+// let count = 0;
+
+// let increase = function () {
+//   let dice = Math.floor( Math.random() * 6 + 1 );
+//   count++;
+// };
+
+// increase();
+// increase();
+// increase();
+// console.log(count);
+
+// let phone = {
+//   brand: 'iphone',
+//   model: '12 pro',
+//   ram: '6gb',
+//   battery: '4000mAh'
+// };
+
+// let Laptop = {
+//   brand: 'Asus',
+//   model: 'Zephyrus',
+//   price: 3000,
+//   discount: 10,
+//   getFullName: function () {
+//     return this.brand + ' ' + this.model;
+//   }
+// };
+
+// console.log( Laptop.getFullName() )
+
